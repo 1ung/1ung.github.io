0b98beac7863d0799c64a8688a62c5a91c3aef67
Erwin Lee <leewhe@hotmail.com>
2018-11-08 19:29:33 +0800

    First commit

---
+++ precache-manifest.24482fcea41518f5535c23a17b2bca78.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4ed642b19d77313be3d3",
+    "url": "/static/css/main.af881633.chunk.css"
+  },
+  {
+    "revision": "4ed642b19d77313be3d3",
+    "url": "/static/js/main.4ed642b1.chunk.js"
+  },
+  {
+    "revision": "859334e7caee21223981",
+    "url": "/static/js/1.859334e7.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9e39dd0e38bcd231e26c0e628d74b4ab",
+    "url": "/index.html"
+  }
+];
